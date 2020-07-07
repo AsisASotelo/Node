336db83185f83cec28d1cc93677abef8530f86f0
AsisASotelo <asis.a.sotelo@gmail.com>
2020-07-06 19:21:58 -0700

    Add npm_intro

---
+++ npm_intro/index.js
@@ -0,0 +1,12 @@
+var _ = require('underscore'); 
+
+// The require function goes through the following steps:
+// Core Module Assumption
+// File or Folder It will check the argument for ./
+// node_modules
+
+// Usage of underscore.contains()
+
+var result = _.contains([1,2,3],2)
+
+console.log(result)
